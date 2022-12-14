56cea19abc67b1e011c0434c877aad1e3f51d08b
randolpho <randolpho@gmail.com>
2022-12-14 00:50:37 -0600

    hello world

---
+++ slideshow/slideshow/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace slideshow.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ slideshow/slideshow/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace slideshow.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
